eebc93e95baf429a6137401fef7ca15a28ec320f
Hildhan123 <hildhan123@gmail.com>
2022-10-11 20:46:27 +0700

    test

--- timer/mqtt.js
+++ timer/mqtt.js
@@ -26,14 +26,73 @@ client.on('connect', () => {
     }
   })
 })
-var a;
-var b;
-client.on('message', (topic, payload) => {
-  let a = payload.toString();
-  let b = topic
-  return a,b;
-})
 
-function payload(a,b) {
-  return console.log('Received message: ', b, a);
+// window.onload = function () {
+//   mqtt = document.getElementById("mqtt");
+// }
+
+// client.on('message', (topic, payload) => {
+//   console.log("Received Message: ", topic, payload);
+//   mqtt.innerHTML += 'Connection failed!<br />';
+// })
+
+
+// var a;
+// var b;
+// client.on('message', (topic, payload) => {
+//   let a = payload.toString();
+//   let b = topic
+//   return console.log("Received Message: ", topic, payload);
+// })
+
+// function payload(a,b) {
+//   return console.log('Received message: ', b, a);
+// }
+
+// function kirim(){
+//   // b=2,a
+//   // return a,b;
+//   // client.on('message', (topic, payload) => {
+   
+//   //   return console.log("Received Message: ", topic, payload);
+//   // })
+// }
+
+const http = require("http");
+var fs = require('fs');
+const { payload } = require('@hapi/hapi/lib/validation')
+
+const host1 = 'localhost';
+const port1 = 8000;
+
+const express = require("express");
+const requestListener = express();
+requestListener.use(express.static("timer"));
+
+// app.get('/', (req, res) => {
+//   res.sendFile('index.html');
+// })
+
+requestListener.get('/', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});  
+client.on('message', (topic, payload) => {
+ res.write("sensor: "+topic + payload)
+})  
+    // res.end("My first server!");
+});
+
+function test()
+{
+client.on('message', (topic, payload) => {
+      return console.log("Received Message: ", topic, payload);
+    })
 }
+
+function loop(payload){
+  var payl = payload;
+}
+
+const server = http.createServer(requestListener);
+server.listen(port1, host1, () => {
+    console.log(`Server is running on http://${host1}:${port1}`);
+});
